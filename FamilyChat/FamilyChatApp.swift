d931d1cabd13537fdef5cf453db5f346a4a6f629
ethemyer <ethemyer07@gmail.com>
2025-04-28 23:06:11 +0300

    Initial Commit

---
+++ FamilyChat/FamilyChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyChatApp.swift
+//  FamilyChat
+//
+//  Created by Ethem Yer on 28.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct FamilyChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
