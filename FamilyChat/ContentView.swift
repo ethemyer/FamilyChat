1328c3f02b6d6bfbdd14d6ce9bd9eaef926c9388
Ethem <ethemyer07@gmail.com>
2025-05-13 08:33:42 +0300

    updated

--- FamilyChat/ContentView.swift
+++ FamilyChat/ContentView.swift
@@ -7,15 +7,64 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State var Mail: String = ""
+    @State var Sifre: String = ""
+    
+    
+    @FocusState private var isYaziKlavyesiGosterilmis: Bool
+
+    
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Text("FamilyChat")
+                    .fontWeight(.bold)
+                    .font(.largeTitle)
+                    .foregroundStyle(.red)
+                Spacer()
+                
+                TextField("Mailinizi giriniz...", text: $Mail)
+                    .keyboardType(.emailAddress)
+                    .focused($isYaziKlavyesiGosterilmis)
+                
+                TextField("Şifrenizi giriniz...", text: $Sifre)
+                    .focused($isYaziKlavyesiGosterilmis)
+                
+                NavigationLink {
+                    RegisterView()
+                } label: {
+                    Text("Kayıt Ol")
+                        .frame(width: 300, height: 50)
+                        .background(Color.blue)
+                        .foregroundStyle(.white)
+                        .cornerRadius(20)
+                }
+                
+                
+//                Button("Kayıt Ol") {
+////                    RegisterView()
+//                }
+//                .frame(width: 300, height: 50)
+//                .background(Color.blue)
+//                .foregroundStyle(.white)
+//                .cornerRadius(20)
+                
+                Spacer()
+                
+                Button("Giriş Yap") {
+                    
+                }
+                .frame(width: 300, height: 50)
+                .background(Color.blue)
+                .foregroundStyle(.white)
+                .cornerRadius(20)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
--- FamilyChat/CoreViews/RegisterView.swift
+++ FamilyChat/CoreViews/RegisterView.swift
@@ -8,10 +8,46 @@
 import SwiftUI
 
 struct RegisterView: View {
+    
+    @State var Mail: String = ""
+    @State var Sifre: String = ""
+    
+    @FocusState private var isYaziKlavyesiGosterilmis: Bool
+    
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            Text("FamilyChat")
+                .fontWeight(.bold)
+                .font(.largeTitle)
+                .foregroundStyle(.red)
+           
+            
+            TextField("Mailinizi giriniz...", text: $Mail)
+                .keyboardType(.emailAddress)
+                .focused($isYaziKlavyesiGosterilmis)
+            
+            TextField("Şifrenizi giriniz...", text: $Sifre)
+                .focused($isYaziKlavyesiGosterilmis)
+            
+            
+            Button("Kayıt Ol") {
+                
+            }
+            .frame(width: 300, height: 50)
+            .background(Color.blue)
+            .foregroundStyle(.white)
+            .cornerRadius(20)
+           
+        }
+        .padding()
+        .padding(.top, 100)
+        
     }
 }
+
 
 #Preview {
     RegisterView()
